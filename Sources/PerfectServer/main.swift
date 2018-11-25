3c771be3bb3225b1b0bb227067b38ab2d384205b
JohnMWood <jmw.home@gmail.com>
2018-11-24 17:46:37 -0700

    Implemented simple Postgres interaction with local database

--- Sources/PerfectServer/main.swift
+++ Sources/PerfectServer/main.swift
@@ -2,12 +2,30 @@ import PerfectHTTP
 import PerfectHTTPServer
 import PerfectPostgreSQL
 
+//do {
+//let status = p.connectdb("host=localhost dbname=PerfectServer")
+//    defer {
+//        p.close() // close the connection
+//    }
+//} catch {
+//    fatalError("\(error)") // fails to make connection to postgresDB
+//}
+let p = PGConnection()
+let status = p.connectdb("host=localhost dbname=PerfectServer")
+let result = p.exec(
+    statement: """
+        SELECT first_name
+        FROM users
+        WHERE id = 1;
+    """)
+let name = result.getFieldString(tupleIndex: 0, fieldIndex: 0)
+
 // create routes for server
 var routes = Routes()
 routes.add(method: .get, uri: "/") {
     request, response in
     response.setHeader(.contentType, value: "text/html")
-    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, \(name ?? "user name not found")!</body></html>")
         .completed()
 }
 
